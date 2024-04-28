3fb60bff94d21746d65af1e7ff5b8ec29826c804
pythcoiner <pythcoiner@proton.me>
2024-04-28 07:31:23 -0500

    fix width/height invertion

--- image_processing/main.cpp
+++ image_processing/main.cpp
@@ -141,7 +141,8 @@ std::vector<cv::Rect> find_items(cv::Mat* mask, bool debug) {
         cv::approxPolyDP(contours[i], contours_poly, 3, true);
 
         // Get the bounding rectangle
-        items[i] = cv::boundingRect(contours_poly);
+        items[i] = cv::boundingclear
+        Rect(contours_poly);
     }
 
     if (debug) {
--- image_processing/src/item.cpp
+++ image_processing/src/item.cpp
@@ -5,9 +5,9 @@
 Item::Item(cv::Scalar color, cv::Rect bounding_rect):
            color(std::move(color)), bounding_rect(bounding_rect)  {
     this->top = this->bounding_rect.y;
-    this->bottom = this->bounding_rect.y + this->bounding_rect.width;
+    this->bottom = this->bounding_rect.y + this->bounding_rect.height;
     this->left = this->bounding_rect.x;
-    this->right = this->bounding_rect.x + this->bounding_rect.height;
+    this->right = this->bounding_rect.x + this->bounding_rect.width;
 }
 
 std::string Item::to_json() {
